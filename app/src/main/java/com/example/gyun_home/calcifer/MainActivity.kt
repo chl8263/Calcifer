2200123b929ee508b63f9cf0309604f6b3627efd
gyun-home <chl8263@naver.com>
2018-11-10 22:52:41 +0900

    welcome chatbot

--- app/src/main/java/com/example/gyun_home/calcifer/MainActivity.kt
+++ app/src/main/java/com/example/gyun_home/calcifer/MainActivity.kt
@@ -12,12 +12,17 @@ import android.text.TextUtils
 import android.util.Log
 import com.example.gyun_home.calcifer.adapter.MessageDTO
 import com.example.gyun_home.calcifer.adapter.RecyclerViewAdapter
+import com.google.firebase.firestore.FirebaseFirestore
 import kotlinx.android.synthetic.main.activity_main.*
+import java.text.SimpleDateFormat
+import java.util.*
 
 class MainActivity : AppCompatActivity() {
 
+    var dateFormatFromString = SimpleDateFormat("yyyy-MM-dd")
+
     var messageDTOs = arrayListOf<MessageDTO>()
-    var aiDataService : AIDataService? = null
+    var aiDataService: AIDataService? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -25,21 +30,21 @@ class MainActivity : AppCompatActivity() {
 
         recyclerView.adapter = RecyclerViewAdapter(messageDTOs)
         recyclerView.layoutManager = LinearLayoutManager(this)
-        var config = AIConfiguration("5fd84bf2f6074a85b4c781c55eb6b381",AIConfiguration.SupportedLanguages.Korean)
+        var config = AIConfiguration("5fd84bf2f6074a85b4c781c55eb6b381", AIConfiguration.SupportedLanguages.Korean)
         aiDataService = AIDataService(config)
 
         button.setOnClickListener {
-            if(!TextUtils.isEmpty(editText.text)){
-                messageDTOs.add(MessageDTO(true,editText.text.toString()))
+            if (!TextUtils.isEmpty(editText.text)) {
+                messageDTOs.add(MessageDTO(true, editText.text.toString()))
                 recyclerView.adapter.notifyDataSetChanged()
-                recyclerView.smoothScrollToPosition(messageDTOs.size-1)
+                recyclerView.smoothScrollToPosition(messageDTOs.size - 1)
                 TalkAsyncTask().execute(editText.text.toString())
                 editText.setText("")
             }
         }
     }
 
-    inner class TalkAsyncTask : AsyncTask<String , Void , Result>(){
+    inner class TalkAsyncTask : AsyncTask<String, Void, Result>() {
         override fun doInBackground(vararg p0: String?): Result? {
             var aiRequest = AIRequest()
             aiRequest.setQuery(p0[0])
@@ -48,16 +53,69 @@ class MainActivity : AppCompatActivity() {
 
         override fun onPostExecute(result: Result?) {
             super.onPostExecute(result)
-            if(result != null){
+            if (result != null) {
                 makeMessage(result)
             }
         }
     }
 
-    fun makeMessage(result:Result){
-        var speech = result.fulfillment.speech
-        messageDTOs.add(MessageDTO(false,speech))
-        recyclerView.adapter.notifyDataSetChanged()
-        recyclerView.smoothScrollToPosition(messageDTOs.size-1)
+    fun makeMessage(result: Result) {
+        when (result.metadata.intentName) {
+            "Schedule" -> {
+                var date = result.parameters["date"]?.asString
+                var dateFromString = dateFormatFromString.parse(date)
+
+                var cal = Calendar.getInstance()
+                cal.time = dateFromString
+                var dayOfWeek = cal.get(Calendar.DAY_OF_WEEK)
+                scheduleMessage(dayOfWeek)
+            }
+            else -> {
+                var speech = result.fulfillment.speech
+                messageDTOs.add(MessageDTO(false, speech))
+                recyclerView.adapter.notifyDataSetChanged()
+                recyclerView.smoothScrollToPosition(messageDTOs.size - 1)
+            }
+        }
+
+    }
+
+    fun scheduleMessage(dayofweek : Int?){
+        var dayOfWeekString : String? = null
+        when (dayofweek){
+            1->{
+                dayOfWeekString = "일요일"
+            }
+            2->{
+                dayOfWeekString = "월요일"
+            }
+            3->{
+                dayOfWeekString = "화요일"
+            }
+            4->{
+                dayOfWeekString = "수요일"
+            }
+            5->{
+                dayOfWeekString = "목요일"
+            }
+            6->{
+                dayOfWeekString = "금요일"
+            }
+            7->{
+                dayOfWeekString = "토요일"
+            }
+        }
+        FirebaseFirestore.getInstance().collection("schedules").whereEqualTo("datofweek",dayOfWeekString).get().addOnCompleteListener { 
+            task ->
+            if(task.isSuccessful){
+                for(document in task.result){
+                    var message = dayOfWeekString + "의 수업은 "+document.data["lecture"]+"입니다."
+                    messageDTOs.add(MessageDTO(false,message))
+                    recyclerView.adapter.notifyDataSetChanged()
+                    recyclerView.smoothScrollToPosition(messageDTOs.size-1)
+                    break
+                }
+            }
+        }
     }
 }
